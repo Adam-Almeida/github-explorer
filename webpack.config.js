305536b33c55098a5fe0cf2b58168d3c0a745650
adam-almeida <adamportifolio@gmail.com>
2022-07-15 23:37:03 -0300

    Github Explorer projeto Rocketseat Ignite

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public')
+    },
+    hot: true,
+    port: 3333
+  },
+
+  plugins:[
+
+    new ReactRefreshWebpackPlugin(),
+
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html')
+    })
+    
+  ].filter(Boolean),
+ 
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: {
+              loader: require.resolve('babel-loader'),
+            options: {
+              plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+            }
+            }
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+};
